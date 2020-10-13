1966606487b15d0c9b110568098f2bdf19acfa1f
Abdoul <agsdpeace@gmail.com>
2020-10-13 19:36:27 +0200

    set up docker, postgres, pytest

---
+++ app/devk/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DevkConfig(AppConfig):
+    name = 'devk'
---
+++ app/tests/test_foo.py
@@ -0,0 +1,23 @@
+import json
+
+from django.urls import reverse
+
+
+def test_hello_world():
+    assert 'hello' == 'hello'
+    assert 'foo' != 'bar'
+
+
+def test_ping(client):
+    # Given
+    # client
+
+    # When
+    url = reverse('ping')
+    response = client.get(url)
+    content = json.loads(response.content)
+
+    # Then
+    assert response.status_code == 200
+    assert content['ping'] == 'pong!'
+
