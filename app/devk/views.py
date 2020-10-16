b62059831a4aa94bf41d389abe4d4080b0efc910
Abdoul <agsdpeace@gmail.com>
2020-10-16 12:34:25 +0200

    Support for DELETE and PUT

--- app/devk/views.py
+++ app/devk/views.py
@@ -26,9 +26,22 @@ class TeamDetail(APIView):
         try:
             return Team.objects.get(pk=pk)
         except Team.DoesNotExist:
-            return Http404
+            raise Http404
 
     def get(self, request, pk, format=None):
         team = self.get_object(pk)
         serializer = TeamSerializer(team)
         return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        team = self.get_object(pk)
+        serializer = TeamSerializer(team, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        team = self.get_object(pk)
+        team.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- app/tests/devk/test_views.py
+++ app/tests/devk/test_views.py
@@ -85,3 +85,64 @@ def test_get_all_team(client, add_team):
     assert resp.status_code == 200
     assert resp.data[0]['name'] == team_one.name
     assert resp.data[1]['name'] == team_two.name
+
+
+@pytest.mark.django_db
+def test_remove_team(client, add_team):
+    team = add_team(name='Abdoul', position='Developer', biography='simple', linkedin='linkedin', twitter='twitter')
+
+    resp = client.get(f'/api/team/{team.id}/')
+    assert resp.status_code == 200
+    assert resp.data['name'] == 'Abdoul'
+
+    resp_two = client.delete(f'/api/team/{team.id}/')
+    assert resp_two.status_code == 204
+
+    resp_three = client.get(f'/api/team/')
+    assert resp_three.status_code == 200
+    assert len(resp_three.data) == 0
+
+
+@pytest.mark.django_db
+def test_remove_team_incorrect_id(client):
+    resp = client.delete(f'/api/team/101/')
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_team(client, add_team):
+    team = add_team(name='Abdoul', position='Developer', biography='simple', linkedin='linkedin', twitter='twitter')
+
+    resp = client.put(
+        f'/api/team/{team.id}/',
+        {'name': 'Timothee', 'position': 'Developer', 'biography': 'sh', 'linkedin': 'linkedin', 'twitter': 'twitter'},
+        content_type='application/json',
+    )
+    assert resp.status_code == 200
+    assert resp.data['name'] == 'Timothee'
+
+    resp_two = client.get(f'/api/team/{team.id}/')
+    assert resp_two.status_code == 200
+    assert resp_two.data['name'] == 'Timothee'
+
+
+@pytest.mark.django_db
+def test_update_team_incorrect_id(client):
+    resp = client.put(f'/api/team/101/')
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize('add_team, payload, status_code', [
+    ['add_team', {}, 400],
+    ['add_team', {'name': 'Abdoul', 'position': 'Developer'}, 400],
+], indirect=['add_team'])
+def test_update_team_invalid_json(client, add_team, payload, status_code):
+    team = add_team(name='Abdoul', position='Developer', biography='simple', linkedin='linkedin', twitter='twitter')
+
+    resp = client.put(
+        f'/api/team/{team.id}/',
+        payload,
+        content_type='application/json',
+    )
+    assert resp.status_code == status_code
